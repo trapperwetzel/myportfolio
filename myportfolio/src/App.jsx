eca5970511b1c86a8e38bb7e60cd8c154cd8a9cb
t_coding <trapperwetzel@gmail.com>
2025-04-17 23:08:46 -0500

    first commit

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,15 @@
+﻿import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+    return (
+        <div className="bg-green-900 min-h-screen">
+            <h1 className="py-10 custom-slideInUp text-center text-3xl font-bold">Hello! This is my portfolio!</h1>
+            <h2 className="mb-4 custom-slideInUp text-center text-4xl font-bold">Hi, I'm Trapper</h2>
+        </div>
+    )
+}
+
+export default App
