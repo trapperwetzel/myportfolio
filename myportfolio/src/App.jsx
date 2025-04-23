cd9c4cf8c09f4a28e22ba6053ff1a7d518e5b707
t_coding <trapperwetzel@gmail.com>
2025-04-22 20:23:37 -0500

    refactor

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,33 +1,22 @@
-﻿import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
-import Resume from './components/Resume'
-
-import './index.css'
+﻿import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
     return (
-        <BrowserRouter basename = "/myportfolio">
-            <div className="flex flex-col">
-                
-                <main className="flex-grow">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/resume" element={<Resume />} /> 
-                    </Routes>
-                </main>
-                
-            </div>
-        </BrowserRouter>
-    )
+        <div className="flex flex-col">
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/resume" element={<Resume />} />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
-export default App
-
+export default App;
